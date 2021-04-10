ace10674f3dae8beb4fb8ff15bf6324c168a97e1
luvkumarcse <luvkumarcse@gmail.com>
2021-04-10 22:56:33 +0530

    ok

---
+++ RightTrianglePattern.java
@@ -0,0 +1,16 @@
+class RightTrianglePattern
+{
+    public static void main(String ar[])
+    {
+        int i,j,row=5;
+        for(i=0;i<=row;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
