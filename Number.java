4599e0759b43a1cff926c9b8494d9da36a781dc5
luvkumarcse <luvkumarcse@gmail.com>
2021-04-10 10:42:26 +0530

    ok

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Number
+{
+    public static void main(String ar[])
+    {
+        int no;
+        System.out.println("enter a number");
+        Scanner in=new Scanner(System.in);
+        no=in.nextInt();
+        if(no %2 == 0)
+        System.out.println("even number");
+        else
+            System.out.println("Odd number");
+        }
+    }
